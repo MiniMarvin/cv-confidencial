f3a478ce66baacbb16488a8be3573eafdbfc73a8
Caio Gomes <caiogomes34@gmail.com>
2022-03-18 20:05:16 -0300

    fix pdf parsing function

--- pdfparse/pdfparse/handler.py
+++ pdfparse/pdfparse/handler.py
@@ -6,41 +6,30 @@ from getPdfText import parse_pages
 import os
 
 s3 = boto3.client('s3')
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
+logging.getLogger().setLevel(logging.INFO)
 
 # TODO: receive the s3 event -> download PDF -> confidentialize -> save on another bucket
 def pdf_parse(event, context):
+    logging.info(event)
     for record in event['Records']:
         bucket_name = record['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(
             record['s3']['object']['key'], encoding='utf-8')
+        file_name = key.split('/')[-1]
         try:
-            logging.info("downloading object: {} | bucket: {}", key, bucket_name)
-            input_file = '/tmp/' + key
+            logging.info(f"downloading object: {key} | bucket: {bucket_name}")
+            input_file = '/tmp/' + file_name
             s3.download_file(bucket_name, key, input_file)
 
-            logging.info("parsing object: {} | bucket: {}", key, bucket_name)
-            output_file = '/tmp/output_'+key
+            logging.info(f"parsing object: {key} | bucket: {bucket_name}")
+            output_file = '/tmp/output_'+file_name
             parse_pages(input_file, output_file)
 
             output_bucket_name = os.environ['CONFIDENTIAL_BUCKET_NAME']
-            logging.info("uploading object: {} | bucket: {}", key, output_bucket_name)
+            logging.info(f"uploading object: {key} | bucket: {output_bucket_name}")
             response = s3.upload_file(output_file, output_bucket_name, key)
-            logging.info("processing finished! object: {} | bucket: {}", key, output_bucket_name)
-            logging.info("response: {} | object: {} | bucket: {}", response, key, output_bucket_name)
+            logging.info(f"processing finished! object: {key} | bucket: {output_bucket_name}")
+            logging.info(f"response: {response} | object: {key} | bucket: {output_bucket_name}")
         except Exception as e:
             # TODO: put data on a queue to reprocess this when the system has stabilized
             logging.error(e)
